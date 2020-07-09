2712e1c34f52cba3748da44701f3b0ba221a13a0
Jacob <jacobkim0119@gmail.com>
2020-07-09 18:57:46 -0500

    Updates

---
+++ precache-manifest.a3ff7b7c8a7c79965bdfaec54c60dacf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9506d6106ec9b36d809",
+    "url": "/static/css/main.edfb434a.chunk.css"
+  },
+  {
+    "revision": "f9506d6106ec9b36d809",
+    "url": "/static/js/main.f9506d61.chunk.js"
+  },
+  {
+    "revision": "2ab911c6c3a96c253593",
+    "url": "/static/js/1.2ab911c6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71fa438ecc49974c1af4a35aa3626fa9",
+    "url": "/static/media/spin.71fa438e.svg"
+  },
+  {
+    "revision": "febae3c6a394c8a50fd5f4a788b28af4",
+    "url": "/static/media/bars.febae3c6.svg"
+  },
+  {
+    "revision": "b61fecc3e00410ddbaa02a2fde8718ae",
+    "url": "/static/media/puff.b61fecc3.svg"
+  },
+  {
+    "revision": "3fd8845e8372f00349f61b6b1c24007d",
+    "url": "/static/media/dots.3fd8845e.svg"
+  },
+  {
+    "revision": "b098170c2775fe2b5853fd7a436de72e",
+    "url": "/static/media/coronavirus.b098170c.svg"
+  },
+  {
+    "revision": "89ebc0e5303d48413ecb509fc88e1393",
+    "url": "/index.html"
+  }
+];
